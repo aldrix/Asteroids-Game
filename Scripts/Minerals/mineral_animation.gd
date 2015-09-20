592a92425158f80ad066a9e1dfa411ae30642210
leotms <martinezazuaje@gmail.com>
2015-09-20 17:38:27 -0430

    Agregadas las escenas para los Khalums y sus animaciones.

--- Scripts/Minerals/mineral_animation.gd
+++ Scripts/Minerals/mineral_animation.gd
@@ -1,16 +1,4 @@
-extends AnimatedSprite
-
-var time = 0
+extends Sprite
 
 func _ready():
-	set_process(true)
-   
-func _process(delta):
-   
-	time += delta
-	if (time > 0.3):
-		if(get_frame() == self.get_sprite_frames().get_frame_count()-1):
-			set_frame(0)
-		else:
-			self.set_frame(get_frame()+ 1)
-		time = 0
+	get_node("AnimationPlayer").play("Spin")
